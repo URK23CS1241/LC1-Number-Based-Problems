4a1648c879848fe1176b18f6ea17711aa55c9864
CHRISTINA JOY P <153481209+URK23CS1241@users.noreply.github.com>
2026-07-20 10:25:45 +0530

    Create Activity 1 – Rotate Values of a, b, and c..java

---
+++ Activity 1 – Rotate Values of a, b, and c..java
@@ -0,0 +1,22 @@
+package com.activity;
+
+public class Activity1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" URK23CS1241");
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        int temp = a;
+        a = c;
+        c = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
