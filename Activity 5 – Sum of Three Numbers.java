dfed932ee1df3effbf18b163ed23c5a0052876a4
CHRISTINA JOY P <153481209+URK23CS1241@users.noreply.github.com>
2026-07-20 10:27:42 +0530

    Create Activity 5 – Sum of Three Numbers.java

---
+++ Activity 5 – Sum of Three Numbers.java
@@ -0,0 +1,28 @@
+package com.activity;
+
+import java.util.Scanner;
+
+public class Activity5 {
+
+    public static void main(String[] args) {
+
+        System.out.println("URK23CS1241");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number : ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number : ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter third number : ");
+        int c = sc.nextInt();
+
+        int sum = a + b + c;
+
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
